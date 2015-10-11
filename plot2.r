e95e0f8497205c00242df84308f040df5eda7643
stephen <stephen.ra.tucker@gmail.com>
2015-10-11 11:14:06 +0100

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,21 +1,19 @@
+## Clean the workspace
+rm(list=ls())
 
 ## Load Data
-fulldata <- read.table( "C:/Users/919647/Documents/PersonalDev/R/household_power_consumption.txt", sep=";" , header=TRUE, stringsAsFactors=FALSE)
-##Convert to Date time here?
+fulldata <- read.table( "/Users/itstuckertime/documents/coursera/household_power_consumption.txt", sep=";" , header=TRUE, stringsAsFactors=FALSE)
 
 newdata <- fulldata[ which(fulldata$Date == "1/2/2007" | fulldata$Date =="2/2/2007"), ]
 
-#Attach subset
-attach(newdata)
 
 ##Create dataset for histograph
-date <- as.Date(newdata$Date, format= "d/m/%y")
-datet<- paste(date, newdata$Time, sep=" ")
-datetime <- as.POSIXct(datet)
+datet<- paste(newdata$Date, newdata$Time, sep=" ")
+datetime <- as.POSIXct(strptime(datet, "%d/%m/%Y %H:%M:%S"))
 globalActivePower<- as.numeric(newdata$Global_active_power)
+
 ##Create PNG
 png("plot2.png", width=480, height=480)
-plot(datetime, globalActivePower, type="1", xlab="", ylab = "Global Active Power (kilowatts)")
+plot(datetime, globalActivePower, type="l", xlab="", ylab = "Global Active Power (kilowatts)")
 
 dev.off()
-
